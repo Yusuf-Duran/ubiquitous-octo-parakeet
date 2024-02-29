e8b1b72e1bc616a549986637e9a3d4f6be0c03a5
Yusuf <firatyusuf.d@gmail.com>
2024-02-29 23:10:14 +0100

    Added second flake stuff

--- flake.nix
+++ flake.nix
@@ -4,26 +4,36 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
     
-      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      nixosConfigurations = {
+        default = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
           modules = [ 
             ./configuration.nix
             inputs.home-manager.nixosModules.default
           ];
         };
-
+        surface = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [
+            ./configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+      };
     };
 }
