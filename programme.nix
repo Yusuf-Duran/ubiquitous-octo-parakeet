29f081b9577d78d68aec7bd60d806500370cf46a
Yusuf <firatyusuf.d@gmail.com>
2024-02-29 16:18:13 +0100

    nixos innit

---
+++ programme.nix
@@ -0,0 +1,27 @@
+
+
+
+
+{lib, config, pkgs, ..}:
+{
+  options = {
+    programme.enable
+      = lib.mkEnableOption "enable programme module"; 
+  };
+
+  config = lib.mkIf config.programme.enable {
+    environment.systemPackages = with pkgs; [
+      vim
+      wget
+      curl
+      discord
+      neovim
+      waybar
+      rofi-wayland
+      kitty
+      fish
+      spotify
+    ]
+  };
+
+}
