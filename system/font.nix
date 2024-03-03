789512475ad858f890bfb85b824c58c316e0d056
Yusuf <firatyusuf.d@gmail.com>
2024-03-03 15:29:38 +0100

    add more nerdfonts

--- system/font.nix
+++ system/font.nix
@@ -10,7 +10,7 @@
     fonts.packages = with pkgs; [
       noto-fonts
       noto-fonts-emoji
-      (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+      (nerdfonts.override { fonts = [ "Noto" "JetBrainsMono" "FiraCode" "DroidSansMono" ]; })
     ];
   };
 }
