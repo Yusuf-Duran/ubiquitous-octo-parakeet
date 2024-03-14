8d84df98e8de5bcd5c11adf8767265d0cc77af95
Yusuf <firatyusuf.d@gmail.com>
2024-03-14 22:17:05 +0100

    Add basic nixvim config

--- home/neovim.nix
+++ home/neovim.nix
@@ -4,5 +4,57 @@
     inputs.nixvim.homeManagerModules.nixvim
   ];
 
-  programs.nixvim.enable = true;
+  programs.nixvim = {
+    enable = true;
+
+    colorschemes.catppuccin = {
+      enable = true;
+      flavour = "mocha";
+    };
+
+    plugins = {
+      lualine = {
+        enable = true;
+      };
+      telescope = {
+        enable = true;
+      };
+      rainbow-delimiters = {
+        enable = true;
+      };
+      treesitter = {
+        enable = true;
+      };
+      autoclose = {
+        enable = true;
+      };
+      barbecue = {
+        enable = true;
+      };
+      chadtree = {
+        enable = true;
+        view.openDirection = "left";
+      };
+      lsp = {
+        enable = true;
+        servers = {
+          nil_ls.enable = true; # nix
+          bashls.enable = true; # bash
+          clangd.enable = true; # c, c++
+          eslint.enable = true; # javascript
+          hls.enable = true; # haskell
+          jsonls.enable = true; # json
+          prolog-ls.enable = true; # prolog
+          pylsp.enable = true; # python
+          texlab.enable = true; # latex
+          tsserver.enable = true; # typescript
+          volar.enable = true; # vue
+        };
+      };
+    };
+
+    viAlias = true;
+    vimAlias = true;
+    withNodeJs = true;
+  };
 }
